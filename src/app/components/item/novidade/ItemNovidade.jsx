450e17b77dd3661fad4036d6f000c598a15f43a4
assimiu <assimium2@gmail.com>
2025-01-17 10:28:14 +0100

    terceiro

--- src/app/components/item/novidade/ItemNovidade.jsx
+++ src/app/components/item/novidade/ItemNovidade.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+/*import Image from "next/image";
 import "./itemNovidade.css"
 
 export default function ItemNovidade({largura}) {
@@ -9,6 +9,30 @@ export default function ItemNovidade({largura}) {
             <div className="conteiner-info">
                 <h3 className="titulo">Ziara Quebo 12-01-2025</h3>
                 <p className="descricao">A ziara occorreu no dia 12 do mes de janeiro de 2025</p>
+            </div>
+        </div>
+    );
+}*/
+
+import Image from "next/image";
+import "./itemNovidade.css";
+
+export default function ItemNovidade({ largura }) {
+    return (
+        <div className="container-novidade" style={{ maxWidth: largura }}>
+            <Image 
+                className="image" 
+                src="/img/img.jpg" 
+                alt="Imagem de Novidade" 
+                width={largura - 30} 
+                height={largura / 1.334} 
+                priority 
+            />
+            <div className="container-info">
+                <h3 className="title">Ziara Quebo 12-01-2025</h3>
+                <p className="description">
+                    A ziara ocorreu no dia 12 do mês de janeiro de 2025.
+                </p>
             </div>
         </div>
     );
--- src/app/components/navbar/NavBar.jsx
+++ src/app/components/navbar/NavBar.jsx
@@ -1,21 +1,40 @@
-import "./navBar.css"
+import { useState } from "react";
+import "./navBar.css";
 import Image from "next/image";
+
 export default function NavBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div className="conteiner-nav-bar">
-            <div className="conteiner-logo">
-                <Image className="logo" src="/logos/logstore.png" alt="GuineStore" width={70} height={60}/>
-                <h4 className="titulo-logo"><a href="/">GuinéStore</a></h4>
+        <nav className="container-nav-bar">
+            <div className="container-logo">
+                <Image
+                    className="logo"
+                    src="/logos/logstore.png"
+                    alt="GuineStore"
+                    width={70}
+                    height={60}
+                />
+                <h4 className="title-logo">
+                    <a href="/">GuinéStore</a>
+                </h4>
             </div>
-            
-            <ul className="nav-bar">
+
+            <button className="hamburger-menu" onClick={toggleMenu}>
+                ☰
+            </button>
+
+            <ul className={`nav-bar ${isMenuOpen ? "open" : ""}`}>
                 <li><a href="/">Início</a></li>
-                <li><a href="/">Novidade</a> </li>
-                <li><a href="/">Seccao</a></li>
+                <li><a href="/">Novidade</a></li>
+                <li><a href="/">Seção</a></li>
                 <li><a href="/">Sobre</a></li>
-                <li><a href="/">Contacto</a></li>
+                <li><a href="/">Contato</a></li>
             </ul>
-            
-        </div>
+        </nav>
     );
 }
